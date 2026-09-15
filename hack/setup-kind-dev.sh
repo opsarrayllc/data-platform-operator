8220e2a058c38e4727a6790ffd9621e2a4ce4ec3
Tim Mahoney <tim+tm@timothymahoney.com>
2026-09-15 14:59:00 -0400

    feat: minio ingress

--- hack/setup-kind-dev.sh
+++ hack/setup-kind-dev.sh
@@ -14,8 +14,8 @@ KUBECTL_BIN="${KUBECTL:-kubectl}"
 CERT_DIR="${KIND_CERT_DIR:-${ROOT}/bin/certs}"
 KUBECONFIG_FILE="${KIND_KUBECONFIG:-${ROOT}/bin/kubeconfig-${CLUSTER}}"
 TLS_SECRET="${KIND_TLS_SECRET:-data-platform-tls}"
-NAMESPACES=(keycloak lakekeeper trino openfga flink superset)
-HOSTS=(keycloak lakekeeper trino openfga opa flink superset)
+NAMESPACES=(keycloak lakekeeper trino openfga flink superset minio)
+HOSTS=(keycloak lakekeeper trino openfga opa flink superset minio)
 
 abs_from_root() {
 	case "$1" in
